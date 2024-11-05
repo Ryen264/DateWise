496261f4210bc37282ef8e1654703003337efbed
THẮNG NGUYỄN QUỐC <nguyenquocthang934@gmail.com>
2024-11-05 16:50:52 +0700

    complete set up frontend - Reactjs

---
+++ my_web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="background-image">
+          <h1>DateWise</h1>
+          <p>Pick your wonderful land</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
