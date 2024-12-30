ed8886189d1913e00987b41091a9ede00c15e552
THẮNG NGUYỄN QUỐC <nguyenquocthang934@gmail.com>
2024-12-30 21:46:15 +0700

    edit profile box

--- DateWiseUI/profile/script.js
+++ DateWiseUI/profile/script.js
@@ -11,3 +11,55 @@ function toggleHeart(button) {
     }
 }
 
+// Get the modal
+var modal = document.getElementById("editModal");
+
+// Function to open the modal
+function openEditModal() {
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeEditModal() {
+    modal.style.display = "none";
+}
+
+// Function to save changes
+function saveChanges() {
+   // Implement saving logic here
+    closeEditModal(); // Close modal after saving
+}
+
+// Function to load profile image preview
+function loadProfileImage(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+    
+  reader.onload = function(e) {
+        document.getElementById('profile-image-preview').src = e.target.result;
+  }
+  if (file) {
+      reader.readAsDataURL(file);
+  }
+}
+
+// Function to remove profile image
+function removeProfileImage() {
+    document.getElementById('profile-image-preview').src = "../asset/image/ava.png";
+     document.getElementById('profile-image-input').value = '';
+}
+function togglePasswordVisibility(inputId) {
+  const passwordInput = document.getElementById(inputId);
+  const passwordToggle = passwordInput.nextElementSibling;
+  const eyeIcon = passwordToggle.querySelector('i');
+
+  if (passwordInput.type === "password") {
+     passwordInput.type = "text";
+      eyeIcon.classList.remove('fa-eye');
+      eyeIcon.classList.add('fa-eye-slash');
+  } else {
+     passwordInput.type = "password";
+      eyeIcon.classList.remove('fa-eye-slash');
+     eyeIcon.classList.add('fa-eye');
+  }
+}
