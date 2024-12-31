330a96ada8e5a672b9ae3e2860449e592de9758e
puiscoding <22127446@student.hcmus.edu.vn>
2024-12-31 16:40:43 +0700

    fix some small bug

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ app.get("/locationdetails", (req, res) => {
 });
 app.post('/submit_onboarding', handle_submit_onboarding);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'Page', 'Homepage', 'homepage.html'));
+  res.redirect('/homepage');
 });
 
 app.get('/getCurrentUser', (req, res) => {
