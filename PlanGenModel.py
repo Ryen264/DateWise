38d5bab9dfe0835a572ab638a24048312688d75e
Ryen_ <107305124+Ryen264@users.noreply.github.com>
2025-01-01 23:52:13 +0700

    Update PlanGenModel.py
    + connect MongoDB before create class
    + delete PlanDataset, merge to Plan class, only read one PLAN document by Plan ID, no load full Plan Dataset
    + update get_collection(), find_latest_id() and get_document_by_id()
    + update init LocationDataset, Plan classes
    + update main logic, find Plan ID by User ID (get latest ID) before creating Plan object

--- PlanGenModel.py
+++ PlanGenModel.py
@@ -3,25 +3,22 @@ import random
 from pymongo import MongoClient
 
 class LocationDataset:
-    def __init__(self, connection_string: str, db_name: str):
-        self.client = MongoClient(connection_string)
-        self.db = self.client[db_name]
-
-        location_list = Utils.get_collection(self.db, 'LOCATION')
+    def __init__(self, database: dict):
+        location_list = Utils.get_collection(database, 'locations')
         self.location_dict = {}
         for record in location_list:
             self.location_dict[str(record['_id'])] = {
-                'district': str(record['LOC_DISTRICT']),
-                'tag': str(record['LOC_TAG']),
-                'price': int(record['LOC_PRICE'])
+                'district': str(record['district']),
+                'tag': str(record['tag']),
+                'price': int(record['price'])
             }
 
-        tag_list = Utils.get_collection(self.db, 'TAG')
+        tag_list = Utils.get_collection(database, 'TAG')
         self.tag_dict = {}
         for record in tag_list:
             self.tag_dict[str(record['_id'])] = str(record['TAG_TYPE'])
 
-        avg_spending_time_list = Utils.get_collection(self.db, 'AVERAGE_SPENDING_TIME')
+        avg_spending_time_list = Utils.get_collection(database, 'AVERAGE_SPENDING_TIME')
         self.avg_spending_time_dict = {}
         for record in avg_spending_time_list:
             self.avg_spending_time_dict[str(record['TYPE_NAME'])] = float(record['TYPE_AVGHOUR'])
@@ -72,141 +69,37 @@ class LocationDataset:
         """
         return list(self.data.keys())
 
-class PlanDataset:
-    def __init__(self, connection_string: str, db_name: str, location_dataset: LocationDataset, avg_time_per_location: float=1.5):
-        self.client = MongoClient(connection_string)
-        self.db = self.client[db_name]
-
-        plan_list = Utils.get_collection(self.db, 'PLAN')
-        self.plan_dict = {}
-        for record in plan_list:
-            self.plan_dict[str(record['_id'])] = {
-                'district': str(record['PLAN_DISTRICT']),
-                'budget': int(record['PLAN_MAXBUDGET']),
-                'start_time': float(record['PLAN_STARTTIME']),
-                'end_time': float(record['PLAN_ENDTIME']),
-                'cuisines': str(record['PLAN_CUISINES']),
-                'mcourses': str(record['PLAN_MCOURSES']),
-                'desserts': str(record['PLAN_DESSERTS']),
-                'activities': str(record['PLAN_ACTIVITES'])
-            }
-
-        self.data = {}
-        for plan_id, plan_info in self.plan_dict.items():
-            lunch_time = Utils.checkin_lunch_time(plan_info['start_time']) or Utils.checkin_lunch_time(plan_info['end_time'])
-            dinner_time = Utils.checkin_dinner_time(plan_info['start_time']) or Utils.checkin_dinner_time(plan_info['end_time'])
-
-            max_locs_num = int((plan_info['end_time'] - plan_info['start_time']) / avg_time_per_location)
-            restaurants_num = lunch_time + dinner_time
-
-            district_locations = [loc for loc in location_dataset.get_locations() if location_dataset.get_district(loc) == plan_info['district']]
-
-            cuisines = Utils.split_string(plan_info['cuisines'], len('CUIS-01'))
-            mcourses = Utils.split_string(plan_info['mcourses'], len('MCOU-01'))
-            desserts = Utils.split_string(plan_info['desserts'], len('DEDR-01'))
-            activities = Utils.split_string(plan_info['activities'], len('ACTI-01'))
-
-            restaurants = [loc for loc in district_locations if location_dataset.get_tag(loc) in cuisines or location_dataset.get_tag(loc) in mcourses]
-            # pick more other restaurants if not enough
-            if len(restaurants) < restaurants_num:
-                for loc in district_locations:
-                    if location_dataset.get_type(loc)=='Restaurant':
-                        restaurants.append(loc)
-
-            others = [loc for loc in district_locations if location_dataset.get_tag(loc) in desserts or location_dataset.get_tag(loc) in activities]
-
-            self.data[plan_id] = {
-                'budget': plan_info['budget'],
-                'start_time': plan_info['start_time'],
-                'end_time': plan_info['end_time'],
-                'hours_sum': plan_info['end_time'] - plan_info['start_time'],
-                'is_lunch_time': lunch_time,
-                'is_dinner_time': dinner_time,
-                'restaurants_num': int(lunch_time + dinner_time),
-                'max_others_num': max_locs_num - restaurants_num,
-                'restaurants': restaurants,
-                'others': others,
-            }
-
-    def get_budget(self, plan_id: str) -> int:
-        """
-        Get budget of a plan.
-        """
-        return self.data[plan_id]['budget']
-    
-    def get_start_time(self, plan_id: str) -> float:
-        """
-        Get start time of a plan.
-        """
-        return self.data[plan_id]['start_time']
-    
-    def get_end_time(self, plan_id: str) -> float:
-        """
-        Get end time of a plan.
-        """
-        return self.data[plan_id]['end_time']
-
-    def get_hours_sum(self, plan_id: str) -> float:
-        """
-        Get sum of hours of a plan.
-        """
-        return self.data[plan_id]['hours_sum']
-    
-    def get_is_lunch_time(self, plan_id: str) -> bool:
-        """
-        Get whether plan time is in lunch time of a plan.
-        """
-        return self.data[plan_id]['is_lunch_time']
-
-    def get_is_dinner_time(self, plan_id: str) -> bool:
-        """
-        Get whether plan time is in dinner time of a plan.
-        """
-        return self.data[plan_id]['is_dinner_time']
-    
-    def get_restaurants_num(self, plan_id: str) -> int:
-        """
-        Get number of restaurants of a plan.
-        """
-        return self.data[plan_id]['restaurants_num']
-    
-    def get_max_others_num(self, plan_id: str) -> int:
-        """
-        Get max number of other locations of a plan.
-        """
-        return self.data[plan_id]['max_others_num']
-    
-    def get_restaurants(self, plan_id: str) -> list[str]:
-        """
-        Get list of satisfied restaurants of a plan.
-        """
-        return self.data[plan_id]['restaurants']
-    
-    def get_others(self, plan_id: str) -> list[str]:
-        """
-        Get list of satisfied other locations of a plan.
-        """
-        return self.data[plan_id]['others']
-    
-    def get_plans(self) -> list[str]:
-        """
-        Get all plans (in ids) in the dataset.
-        """
-        return list(self.data.keys())
-
 class Plan:
-    def __init__(self, plan_id: str, plan_dataset: PlanDataset, location_dataset: LocationDataset, max_pool_size: int=10, budget_time_ratio: float=0.5, budget_prob_threshold: float=0.2, time_threshold: int=0):
+    def __init__(self, plan_id: str, database: dict, location_dataset: LocationDataset, avg_time_per_location: float=1.5, max_pool_size: int=10, budget_time_ratio: float=0.5, budget_prob_threshold: float=0.2, time_threshold: int=0):
         self.id = plan_id
-        self.budget = plan_dataset.get_budget(plan_id)
-        self.start_time = plan_dataset.get_start_time(plan_id)
-        self.end_time = plan_dataset.get_end_time(plan_id)
-        self.hours_sum = plan_dataset.get_hours_sum(plan_id)
-        self.is_lunch_time = plan_dataset.get_is_lunch_time(plan_id)
-        self.is_dinner_time = plan_dataset.get_is_dinner_time(plan_id)
-        self.restaurants_num = plan_dataset.get_restaurants_num(plan_id)
-        self.max_others_num = plan_dataset.get_max_others_num(plan_id)
-        self.restaurants = plan_dataset.get_restaurants(plan_id)
-        self.others = plan_dataset.get_others(plan_id)
+
+        plan_document = Utils.get_document_by_id(plan_id, database, 'PLAN')
+        self.budget = int(plan_document['PLAN_MAXBUDGET'])
+        self.start_time = float(plan_document['PLAN_STARTTIME'])
+        self.end_time = float(plan_document['PLAN_ENDTIME'])
+        self.hours_sum = self.end_time - self.start_time
+        self.is_lunch_time = Utils.checkin_lunch_time(self.start_time) or Utils.checkin_lunch_time(self.end_time)
+        self.is_dinner_time = Utils.checkin_dinner_time(self.start_time) or Utils.checkin_dinner_time(self.end_time)
+        self.restaurants_num = int(self.is_lunch_time + self.is_dinner_time)
+        
+        max_locs_num = int(self.hours_sum / avg_time_per_location)
+        self.max_others_num = max_locs_num - self.restaurants_num
+
+        plan_district = str(plan_document['PLAN_DISTRICT'])
+        district_locations = [loc for loc in location_dataset.get_locations() if location_dataset.get_district(loc) == plan_district]
+
+        plan_cuisines = list(plan_document['PLAN_CUISINES'])
+        plan_mcourses = list(plan_document['PLAN_MCOURSES'])
+        self.restaurants = [loc for loc in district_locations if location_dataset.get_tag(loc) in plan_cuisines or location_dataset.get_tag(loc) in plan_mcourses]
+        # pick more other restaurants if not enough
+        if len(self.restaurants) < self.restaurants_num:
+            for loc in district_locations:
+                if location_dataset.get_type(loc)=='Restaurant':
+                    self.restaurants.append(loc)
+
+        plan_desserts = list(plan_document['PLAN_DESSERTS'])
+        plan_activities = list(plan_document['PLAN_ACTIVITIES'])
+        self.others = [loc for loc in district_locations if location_dataset.get_tag(loc) in plan_desserts or location_dataset.get_tag(loc) in plan_activities]
 
         self.budget_prob_threshold = budget_prob_threshold
         self.time_threshold = time_threshold
@@ -303,11 +196,29 @@ class Plan:
 
 class Utils:
     @staticmethod
-    def get_collection(db: dict, collection_name: str) -> list[dict]:
+    def get_collection(database: dict, collection_name: str) -> list[dict]:
         """
         Get all documents in a collection.
         """
-        return list(db[collection_name].find())
+        return list(database[collection_name].find())
+    
+    @staticmethod
+    def find_latest_id(field_name: str, value: str, collection_name: str, database: dict) -> dict:
+        """
+        Find latest id in a collection having {field: 'value'}.
+        """
+        latest_document = database[collection_name].find_one({field_name: value}, sort=[("_id", -1)])
+
+        return latest_document["_id"]
+    
+    @staticmethod
+    def get_document_by_id(id: str, database: dict, collection_name: str) -> dict:
+        """
+        Get a document in a collection by id.
+        """
+        document = database[collection_name].find_one({"_id": id})
+
+        return document
 
     @staticmethod
     def checkin_time(hours: float, starttime: float, endtime: float) -> bool:
@@ -569,10 +480,6 @@ class Utils:
 
 
 def main():
-    # MongoDB key
-    connection_string = "mongodb+srv://giabao:lenguyengiabao@datewise.hxajp.mongodb.net/?retryWrites=true&w=majority&appName=DateWise"
-    db_name = 'AppData'
-
     # Default parameters
     avg_time_per_location=1.5
     max_pool_size = 10
@@ -580,13 +487,24 @@ def main():
     budget_prob_threshold = 0.2
     time_threshold = 0
 
-    # Load datasets
-    location_dataset = LocationDataset(connection_string, db_name)
-    plan_dataset = PlanDataset(connection_string, db_name, location_dataset, avg_time_per_location)
+    # MongoDB key
+    connection_string = "mongodb+srv://giabao:lenguyengiabao@datewise.hxajp.mongodb.net/?retryWrites=true&w=majority&appName=DateWise"
+    db_name = 'AppData'
 
-    # Press Make a new plan and give plan orders -> Get a Plan ID
-    plan_id = "241112-001030"
-    plan = Plan(plan_id, plan_dataset, location_dataset, max_pool_size, budget_time_ratio, budget_prob_threshold, time_threshold)
+    # Connect MongoDB
+    client = MongoClient(connection_string)
+    database = client[db_name]
+
+    # Load datasets
+    user_id = "USR-001"
+    location_dataset = LocationDataset(database)
+
+    # Press Make a new plan and give plan orders
+    # Find latest Plan ID having {'PLAN_USER': User ID}
+    plan_id = Utils.find_latest_id('PLAN_USER', user_id, 'PLAN', database)
+    # plan_id = '241112-001030'
+    # Create Plan object
+    plan = Plan(plan_id, database, location_dataset, avg_time_per_location, max_pool_size, budget_time_ratio, budget_prob_threshold, time_threshold)
 
     # Press Generate plan and show Plan Detail
     generated_plan_detail = plan.generate_plan(location_dataset)
